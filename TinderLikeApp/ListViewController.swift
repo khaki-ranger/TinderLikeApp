0acca60856e15352e59818b29370c6fc7ac4f186
Terashima <yamanogomora@gmail.com>
2019-03-23 18:55:34 +0900

    Add: 全てのカードの選択が終了した後に遷移する画面を作成

---
+++ TinderLikeApp/ListViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ListViewController.swift
+//  TinderLikeApp
+//
+//  Created by 寺島 洋平 on 2019/03/23.
+//  Copyright © 2019年 YoheiTerashima. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
